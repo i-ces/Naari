0fc6cf7f2d1c97145bf39f38194128476075a31d
bhawana <bhawana.prs@gmail.com>
2020-01-24 11:20:16 +0545

    cronjob for every logged in user

--- cronjob.js
+++ cronjob.js
@@ -1,25 +1,29 @@
-const nodemailer = require("nodemailer");
+const cronjob = email => {
+  const nodemailer = require("nodemailer");
 
-let transporter = nodemailer.createTransport({
-  service: "gmail",
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
 
-  auth: {
-    user: "bhawana.prs@gmail.com",
-    pass: "****"
-  }
-});
+    auth: {
+      user: "bhawana.prs@gmail.com",
+      pass: "*****"
+    }
+  });
 
-mailOptions = {
-  from: "bhawana.prs@gmail.com",
-  to: "bhawana.prs@gmail.com",
-  subject: "Alert for weekly health checkup",
-  text:
-    "Its been a week since you have been  to hospital . Please contact your doctor/gynaecologists"
+  mailOptions = {
+    from: "bhawana.prs@gmail.com",
+    to: email,
+    subject: "Alert for weekly health checkup",
+    text:
+      "Its been a week since you have been  to hospital . Please contact your doctor/gynaecologists"
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("email sent");
+    }
+  });
 };
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("email sent");
-  }
-});
+
+module.exports = { cronjob: cronjob };
