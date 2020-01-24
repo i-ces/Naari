61c941eb48c439cbc8fa32ab65c11e6cd44dfe5d
bhawana <bhawana.prs@gmail.com>
2020-01-24 15:24:35 +0545

    message for every subscribed women

--- sms.js
+++ sms.js
@@ -1,13 +1,18 @@
-const tokens = require("./tokens");
-client = tokens();
-client.messages
-  .create({
-    body:
-      "This website stores cookies on your computer If you decline, your information won’t be tracked when you visit this website. A single cookie will be used in your browser to remember your preference not to be tracked.",
-    from: process.env.FROM,
-    to: process.env.TO
-  })
-  .then(message => console.log(message))
-  .catch(err => {
-    console.log(err);
-  });
+const sms = phoneNumber => {
+  const tokens = require("./tokens");
+  client = tokens();
+  client.messages
+    .create({
+      body:
+        "This website stores cookies on your computer If you decline, your information won’t be tracked when you visit this website. A single cookie will be used in your browser to remember your preference not to be tracked.",
+      from: process.env.FROM,
+      to: phoneNumber
+    })
+    .then(message => console.log(message))
+    .catch(err => {
+      console.log(err);
+    });
+};
+module.exports = {
+  sms: sms
+};
